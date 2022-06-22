2477dc519664b66b797c071b89161201c2bd3cd0
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-06-22 19:57:34 +0530

    Server can send 'hello' response

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const { createServer } = require('net');
+const response = (text) => `HTTP/1.1 200 OK \r\n\r\n${html('hello')}`;
+const html = body => `<html><body>${body}</body></html>`;
+
+const onConnection = (socket) => {
+  socket.setEncoding('utf8');
+
+  socket.on('data', (chunk) => {
+    socket.write(response(chunk));
+    socket.end();
+  });
+};
 
 const main = () => {
   const PORT = 4444;
-  const server = createServer((socket) => {
-    socket.on('data', (chunk) => {
-      console.log(chunk);
-    });
-  });
+  const server = createServer((socket) => onConnection(socket));
 
   server.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
